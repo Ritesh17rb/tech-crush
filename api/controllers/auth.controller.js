0f37b3475c5cc727d14b6faf704d39cdd132b120
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-04-20 10:14:03 +0530

    corrected update controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,7 +50,7 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
 
     if (!validPassword) {
-      return next(errorHandler(400, "Invalid pasword!"));
+      return next(errorHandler(400, "Invalid password!"));
     }
 
     const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
@@ -120,3 +120,155 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const test = (req, res) => {
+  res.json({ message: "Hello from Test API" });
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    // Check if the authenticated user is an admin or the same user whose profile is being updated
+    if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+      return next(errorHandler(403, "Unauthorized"));
+    }
+
+    // Check if the request body contains a password field and validate it
+    if (req.body.password && req.body.password.length < 6) {
+      return next(
+        errorHandler(400, "Password must be at least 6 characters long")
+      );
+    }
+
+    // Check if the request body contains a username field and validate it
+    if (req.body.username) {
+      const username = req.body.username;
+      if (username.length < 7 || username.length > 20) {
+        return next(
+          errorHandler(
+            400,
+            "Username must be between 7 and 20 characters long"
+          )
+        );
+      }
+      if (username.includes(" ")) {
+        return next(errorHandler(400, "Username cannot contain spaces"));
+      }
+      if (username !== username.toLowerCase()) {
+        return next(errorHandler(400, "Username must be lowercase"));
+      }
+      if (!username.match(/^[a-zA-Z0-9]+$/)) {
+        return next(
+          errorHandler(
+            400,
+            "Username must contain only letters and numbers"
+          )
+        );
+      }
+    }
+
+    // Update the user's profile
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+
+    // Omit the password field from the response
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "Unauthorized"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = (req, res, next) => {
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User has been signed out");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "Unauthorized"));
+  }
+
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const { password, ...rest } = user._doc;
+
+    res.status(200).json(rest);
+
+  } catch (error) {
+    next(error);
+  }
+};  
